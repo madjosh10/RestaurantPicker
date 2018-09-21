ad3d065927b7643e1652364e4c092a5f9527729a
madjosh10 <madrigaljosh10@yahoo.com>
2018-09-21 14:59:24 -0500

    Initial Commit

---
+++ RestaurantPick/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RestaurantPick
+//
+//  Created by Joshua Madrigal on 9/21/18.
+//  Copyright © 2018 Joshua Madrigal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
